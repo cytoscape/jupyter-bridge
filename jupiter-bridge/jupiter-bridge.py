8c127c579b6bc34c6b33a1e46fab4faca194733f
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-07-30 17:50:26 -0700

    Prepare to run under uwsgi

--- jupiter-bridge/jupiter-bridge.py
+++ jupiter-bridge/jupiter-bridge.py
@@ -59,7 +59,7 @@ def queue_request():
             if request.content_type.startswith('application/json'):
                 data = request.get_data()
                 message = json.loads(data.decode('utf-8'))
-                return Response('zOKz', status=200)
+
 
                 # Verify that any previous reply has been picked up before trying to send new request
                 reply_status = channel_status[channel]['reply']['status']
@@ -67,6 +67,7 @@ def queue_request():
                     raise Exception(f'Reply not picked up before new request, reply: ' + str(reply_status['message']) + ', request: ' + str(message))
                 channel_status[channel]['reply']['status'] = empty_status.copy()
 
+                return Response('zOKz', status=200)
                 _enqueue('request', channel, message)
                 return Response('', status=200, content_type='text/plain', headers={'Access-Control-Allow-Origin': '*'})
             else:
