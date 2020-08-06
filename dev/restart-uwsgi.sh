a46bb242c60c615a5cd585eb7613663cb45b5e2c
Barry Demchak <idekerlab.bdemchak@gmail.com>
2020-08-06 15:25:34 -0700

    Cleaned up config files, changed name from myproject to jupyter-bridge

--- dev/restart-uwsgi.sh
+++ dev/restart-uwsgi.sh
@@ -1,6 +1,5 @@
-echo When executing this, be sure pwd=/home/bdemchak/jupyter-bridge
-
+echo When executing this, be sure pwd=/home/bdemchak/
 sudo systemctl stop jupyter-bridge
-sudo rm server/jupyter-bridge.log
+sudo rm jupyter-bridge/server/jupyter-bridge.log
 sudo systemctl start jupyter-bridge
 sudo systemctl status jupyter-bridge
