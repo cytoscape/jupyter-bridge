528ce17b6bc1cf48245ed50614220fcdf7603e42
Barry Demchak <bdemchak@ucsd.edu>
2021-06-22 15:40:15 -0700

    Update p4c_init.py

--- client/p4c_init.py
+++ client/p4c_init.py
@@ -1,17 +1,18 @@
-import IPython, importlib, sys
-
-# If Colab, module is installed at system level. If Notebook, module is in user-space
-RunningInCOLAB = 'google.colab' in str(get_ipython())
-ModuleSpace = '' if RunningInCOLAB else '--user'
+import IPython, sys
     
 # Install py4cytoscape module into python space
-if 'py4cytoscape' in sys.modules:
-    importlib.reload(py4cytoscape)
-else:
-    get_ipython().run_line_magic('run', '-m pip install ' + ModuleSpace + ' py4cytoscape')
+if 'py4cytoscape' not in sys.modules: # Check to see if py4cytoscape already loaded
+  # No ... if module name not already defined by user, choose default module name
+  if "_PY4CYTOSCAPE" not in globals(): _PY4CYTOSCAPE = 'py4cytoscape'
+        
+  # If Colab, module is installed at system level. If Notebook, module is in user-space  
+  RunningInCOLAB = 'google.colab' in str(get_ipython())
+  ModuleSpace = '' if RunningInCOLAB else '--user'
+  get_ipython().run_line_magic('run', '-m pip install ' + ModuleSpace + ' ' + _PY4CYTOSCAPE)
+
 import py4cytoscape as p4c
 
 # Start the Jupyter-Bridge to enable communication with Cytoscape on workstation
-print(f'Loading Javascript client ... {p4c.get_browser_client_channel()} on {p4c.get_jupyter_bridge_url()}')
 browser_client_js = p4c.get_browser_client_js(True)
-IPython.display.Javascript(browser_client_js) # Start browser client
+print(f'Loading Javascript client ... {p4c.get_browser_client_channel()} on {p4c.get_jupyter_bridge_url()}')
+IPython.display.Javascript(browser_client_js) # Start browser client'
