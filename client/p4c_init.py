24666f6050559133e16de0a7e30c6446c3b5b97b
Barry Demchak <idekerlab.bdemchak@gmail.com>
2021-06-26 16:39:45 -0700

    Created workarounds for re-execution under Colab

--- client/p4c_init.py
+++ client/p4c_init.py
@@ -16,9 +16,10 @@ import py4cytoscape as p4c
 # Start the Jupyter-Bridge to enable communication with Cytoscape on workstation
 if "_PY4CYTOSCAPE_CHANNEL" in globals():
   if _PY4CYTOSCAPE_RUNNING_IN_COLAB:
-    print('Re-initialization does not work properly in Colab.  Do Runtime | Factory Reset Runtime on the Colab menu, and then restart your notebook.')
+    error = 'Re-initialization does not work properly in Colab.  Do Runtime | Factory Reset Runtime on the Colab menu, and then restart your notebook.'
+    print(error)
     exit()
-    raise Exception
+    raise Exception(error)
   else:
     print(f'Skip reloading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
   _PY4CYTOSCAPE_BROWSER_CLIENT_JS = '' # Add nothing to the browser's Javascript
