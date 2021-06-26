f09913c15a5490ade4bd4d40f813c1aa69c07458
Barry Demchak <idekerlab.bdemchak@gmail.com>
2021-06-26 15:47:03 -0700

    Restructured initialization to avoid re-initializing browser client

--- client/p4c_init.py
+++ client/p4c_init.py
@@ -1,26 +1,25 @@
 import IPython, sys
-    
+
+RunningInCOLAB = 'google.colab' in str(get_ipython())
+
 # Install py4cytoscape module into python space
 if 'py4cytoscape' not in sys.modules: # Check to see if py4cytoscape already loaded
   # No ... if module name not already defined by user, choose default module name
   if "_PY4CYTOSCAPE" not in globals(): _PY4CYTOSCAPE = 'py4cytoscape'
         
   # If Colab, module is installed at system level. If Notebook, module is in user-space  
-  RunningInCOLAB = 'google.colab' in str(get_ipython())
   ModuleSpace = '' if RunningInCOLAB else '--user'
   get_ipython().run_line_magic('run', '-m pip install ' + ModuleSpace + ' ' + _PY4CYTOSCAPE)
 
 import py4cytoscape as p4c
 
-
-class StopExecution(Exception):
-  def _render_traceback_(self):
-    pass
-
 # Start the Jupyter-Bridge to enable communication with Cytoscape on workstation
 if "_PY4CYTOSCAPE_CHANNEL" in globals():
+  if RunningInCOLAB:
+    print('Re-initialization does not work properly in Colab ... restarting Kernel. Restart your notebook.')
+    exit()
   print(f'Skip loading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
-  browser_client_js = ''
+  browser_client_js = '' # Add nothing to the browser's Javascript
 else:
   if "_PY4CYTOSCAPE_DEBUG_BROWSER" not in globals():  _PY4CYTOSCAPE_DEBUG_BROWSER = False
   browser_client_js = p4c.get_browser_client_js(_PY4CYTOSCAPE_DEBUG_BROWSER)
