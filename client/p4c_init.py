da2c2e5cdde147a9b5de1e94f2359edd89b953d3
Barry Demchak <idekerlab.bdemchak@gmail.com>
2021-06-26 16:31:01 -0700

    Restructured initialization to avoid re-initializing browser client

--- client/p4c_init.py
+++ client/p4c_init.py
@@ -16,9 +16,10 @@ import py4cytoscape as p4c
 # Start the Jupyter-Bridge to enable communication with Cytoscape on workstation
 if "_PY4CYTOSCAPE_CHANNEL" in globals():
   if _PY4CYTOSCAPE_RUNNING_IN_COLAB:
-    print('Re-initialization does not work properly in Colab ... restarting Kernel. Restart your notebook.')
+    print('Re-initialization does not work properly in Colab.  Do Runtime | Factory Reset Runtime, and then restart your notebook.')
     exit()
-  print(f'Skip reloading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
+  else:
+    print(f'Skip reloading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
   _PY4CYTOSCAPE_BROWSER_CLIENT_JS = '' # Add nothing to the browser's Javascript
 else:
   if "_PY4CYTOSCAPE_DEBUG_BROWSER" not in globals():  _PY4CYTOSCAPE_DEBUG_BROWSER = False
