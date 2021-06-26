5d93326bb93ca7984fc6aa7fad499598bb92e715
Barry Demchak <idekerlab.bdemchak@gmail.com>
2021-06-26 16:36:24 -0700

    Created workarounds for re-execution under Colab

--- client/p4c_init.py
+++ client/p4c_init.py
@@ -16,8 +16,9 @@ import py4cytoscape as p4c
 # Start the Jupyter-Bridge to enable communication with Cytoscape on workstation
 if "_PY4CYTOSCAPE_CHANNEL" in globals():
   if _PY4CYTOSCAPE_RUNNING_IN_COLAB:
-    print('Re-initialization does not work properly in Colab.  Do Runtime | Factory Reset Runtime, and then restart your notebook.')
+    print('Re-initialization does not work properly in Colab.  Do Runtime | Factory Reset Runtime on the Colab menu, and then restart your notebook.')
     exit()
+    raise Exception
   else:
     print(f'Skip reloading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
   _PY4CYTOSCAPE_BROWSER_CLIENT_JS = '' # Add nothing to the browser's Javascript
@@ -26,5 +27,7 @@ else:
   _PY4CYTOSCAPE_BROWSER_CLIENT_JS = p4c.get_browser_client_js(_PY4CYTOSCAPE_DEBUG_BROWSER)
   _PY4CYTOSCAPE_CHANNEL = p4c.get_browser_client_channel()
   print(f'Loading Javascript client ... {_PY4CYTOSCAPE_CHANNEL} on {p4c.get_jupyter_bridge_url()}')
+  if _PY4CYTOSCAPE_RUNNING_IN_COLAB:
+    print('ADVICE: WHEN RUNNING UNDER COLAB, DO NOT RE-RUN THIS CELL WITHOUT MANUALLY EXECUTING Runtime | Factory Reset Runtime FROM THE COLAB MENU FIRST.')
 
 # Caller must do this part: IPython.display.Javascript(_PY4CYTOSCAPE_BROWSER_CLIENT_JS) # Start browser client'
